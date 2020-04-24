d46ea3b5c4c8e5fad5821e601cb6771fd9b736ab
ns <portification@gmail.com>
2020-04-24 18:55:15 -0400

    bin/rewrite: Remove redundant clone

--- src/bin/rewrite.rs
+++ src/bin/rewrite.rs
@@ -7,7 +7,7 @@ fn main() -> std::io::Result<()> {
     let reader = reader.read_header1()?;
     let reader = reader.read_header2()?;
     let data = reader.into_write_data()?;
-    let mut new_path = path.clone();
+    let mut new_path = path;
     new_path.push_str(".rewrite.brs");
     brs::write_save(&mut File::create(new_path)?, &data)?;
     Ok(())
